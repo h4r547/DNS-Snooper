59054c890523f59875d0d348c3741db3cf157712
bugs <harshadsathaye@gmail.com>
2018-04-26 21:27:37 -0400

    Implemented HTTP Sniffer

--- readpcap.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import *
-
-packets = rdpcap('dns.pcap')
-
-for p in packets:
-    if p.haslayer(DNS):
-        print p[DNS].qd.qname
--- snooper.py
+++ snooper.py
@@ -55,22 +55,54 @@ def clean_up(iface):
 def list_all(pkt):
     if pkt.haslayer(Dot11) and pkt.type == 2:
         try:
-            if 'TCP' in pkt and 'IP' in pkt:
-                wrpcap('tcp.pcap', pkt, append=True)
+            if 'TCP' in pkt and 'IP' in pkt and 'Raw' in pkt:
+                if pkt[4]['TCP'].dport == 80:
+                    wrpcap('http.pcap', pkt, append=True)
+
+                    data = get_data('http',pkt[4]['TCP'][Raw].load)
+                    print "================ HTTP Request ================"
+                    print "Source MAC: "+pkt[1].addr2
+                    print "IP: "+str(pkt[4][IP].src)+" > "+str(pkt[4][IP].dst)
+                    for field in data.keys():
+                        #print field+": "+data[field]
+                        if data[field] == 'POST':
+                            print pkt.show()
+
+                wrpcap('tcp.pcap', pkt)
             elif 'UDP' in pkt and 'IP' in pkt and pkt[4]['UDP'].dport == 53:
-                print "DNS"
-                print str(pkt[4][IP].src)+" > "+str(pkt[4][IP].dst)
+                #print "DNS"
+                #print str("MAC: "+pkt[1].addr2+" IP: "+pkt[4][IP].src)+" > "+str(pkt[4][IP].dst)
                 wrpcap('dns.pcap', pkt)
-                print "Query: "+get_queries()
+                #print "Query: "+get_data('dns')
         except Exception as e:
             print e
 
 
-def get_queries():
-    packets = rdpcap('dns.pcap')
-    for p in packets:
-        if p.haslayer(DNS):
-            return p[DNS].qd.qname
+def get_data(proto, data=None):
+    if proto == 'dns':
+        packets = rdpcap('dns.pcap')
+        for p in packets:
+            if p.haslayer(DNS):
+                return p[DNS].qd.qname
+
+    elif proto == 'http':
+        dict = {}
+        lines = data.split('\n')
+        for line in lines:
+            if 'Host' in line:
+                dict['host'] = line.split(': ')[1]
+            elif 'GET' in line:
+                dict['req'] = 'GET'
+                #dict['requrl'] = dict['host'] + line.split( )[1]
+            elif 'POST' in line:
+                dict['req'] = 'POST'
+                #dict['requrl'] = dict['host'] + line.split( )[1]
+                print data
+            elif 'Referer' in line:
+                dict['ref'] = line.split(': ')[1]
+            elif 'User' in line:
+                dict['uagent'] = line.split(': ')[1]
+        return dict
 
 
 interface = get_iface()
